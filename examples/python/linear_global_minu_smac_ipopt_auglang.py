e711f83a07d2193cc236d543104dae955859f380
Panos Lambrianides <panos@protonomics.com>
2024-10-28 02:06:56 +0000

    Increased number of trials in the linear case'

--- examples/python/linear_global_minu_smac_ipopt_auglang.py
+++ examples/python/linear_global_minu_smac_ipopt_auglang.py
@@ -279,7 +279,7 @@ def do_optimize(initial_conditions):
     count = count + 1
     # Define the configuration space
     cs = ConfigurationSpace(name="vpd config space", space={"p1": Float("p1", bounds=(p10min, p10max), default=p1) })
-    scenario = Scenario(cs, deterministic=False, n_trials=2500)
+    scenario = Scenario(cs, deterministic=False, n_trials=5000)
   
     optimizer = initialize_smac_with_initial_conditions(scenario, initial_conditions)
 
